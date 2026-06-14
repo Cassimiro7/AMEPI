fe9af54ed5666cc8812e6b326972f73006b6b8f8
Cassimiro7 <gustavo.ramos62@aluno.ifce.edu.br>
2026-06-14 15:09:44 -0300

    inseticída

--- frontend/auth.ts
+++ frontend/auth.ts
@@ -3,4 +3,6 @@ import Google from "next-auth/providers/google"
 
 export const { handlers, signIn, signOut, auth } = NextAuth({
   providers: [Google],
+  // 👇 É ESTA LINHA QUE VAI DESTRAVAR O SEU LOGIN NA AWS
+  trustHost: true,
 })
